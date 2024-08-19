d485408f1cdbda41de740ee7814dbab9289bc221
roe-sh <92922959+roe-sh@users.noreply.github.com>
2024-08-19 17:36:51 +0300

    task1

---
+++ task1core/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using task1core.Models;
+
+namespace task1core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+
+        public CategoriesController(MyDbContext context)
+        {
+            _db = context;
+        }
+
+       
+        [HttpGet("getAllCategories")]
+        public IActionResult GetAllCategories()
+        {
+            var categories = _db.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("GetCategoryById/{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _db.Categories.Find(id);
+           
+            return Ok(category);
+        }
+    }
+}
+
---
+++ task1core/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using task1core.Models;
+namespace task1core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+
+        public ProductsController(MyDbContext context)
+        {
+            _db = context;
+        }
+
+       
+        [HttpGet("getAllProducts")]
+        public IActionResult GetAllProducts()
+        {
+            var products = _db.Products.ToList();
+            return Ok(products);
+        }
+
+        // GET: api/Products/{id}
+        [HttpGet("GetProductById/{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _db.Products.Find(id);
+            
+            return Ok(product);
+        }
+    }
+}
+   
